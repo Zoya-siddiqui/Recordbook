ea6857515ebf9b86bec9e97c7c3e778fe0c456ea
your_name <Zoya>
2023-12-18 22:57:51 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+import Header from './components/Header';
+
 
 function App() {
+  const [data, setData] = useState([]);
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const addData = () => {
+    setData([...data, { name, email }]);
+    setname("");
+    setemail("");
+  }
+
+  const removeItem=(index)=>{
+    let arr=data;
+    arr.splice()
+    data.splice(index,1)
+    setData([...arr])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <center>
+        <div className='container my-3 d-flex justify-content-center '>
+          <TextField id="outlined-basic" label="Name" variant="outlined" value={name} onChange={(event) => setname(event.target.value)} />
+          <TextField id="outlined-basic" label="Email" variant="outlined" className='mx-2' value={email} onChange={(event) => setemail(event.target.value)} />
+          <Button variant="contained" className='mx-2' onClick={addData}>Add</Button>
+        </div>
+        <div class="container my-5 w-50 shadow-sm p-3 mb-1 bg-white rounded align-items-center border border-1">
+          <div class="row">
+            <div class="col-sm">
+              <b>NAME</b>
+            </div>
+            <div class="col-sm">
+              <b>EMAIL</b>
+            </div>
+            <div class="col-sm">
+              <b>REMOVE</b>
+            </div>
+          </div>
+        </div>
+        {
+          data.map((ele, index) => {
+            return (
+              <>
+              <div class="container w-50 shadow-sm p-2 mb-1 bg-white rounded align-items-center ">
+                  <div class="row a" key={index}>
+                      <div class="col-sm">
+                          {ele.name}
+                      </div>
+                      <div class="col-sm">
+                          {ele.email}
+                      </div>
+                      <div class="col-sm">
+                          <Button variant="outlined" color="error" onClick={()=>removeItem(index)}>
+                             DELETE
+                          </Button>
+                      </div>
+                  </div>
+              </div >
+          </>
+            )
+
+          })
+        }
+      </center>
+
+    </>
   );
 }
 
